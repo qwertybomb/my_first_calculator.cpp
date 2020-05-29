fd36724ea2be0ad3c348a1c53dd325e061391f72
qwertybomb <42456119+qwertybomb@users.noreply.github.com>
2020-05-29 08:09:48 -0400

    Add files via upload

--- generator.cc
+++ generator.cc
@@ -1,21 +1,21 @@
 #include <iostream>
 #include <fstream>
-void get_formated_string(const size_t& num1,const size_t& num2,const char& op,char resulting_string[],const bool& first) {
+void get_formated_string(const ptrdiff_t& num1,const ptrdiff_t& num2,const char& op,char resulting_string[],const bool& first) {
     //floating point numbers only need to be printed if '/' is being used
     if(op ^ '/'){
-    size_t result = op=='+'?num1+num2:op=='-'?num1-num2:op=='*'?num1*num2:op=='/'?num1/static_cast<double>(num2):-1;
-    sprintf(resulting_string,"%s(num1 == %zu && sign == '%c' && num2 == %zu) \n"
-                       "    std::cout << %zu << '%c' << %zu << \" = \" << %zu << std::endl;\n", first?"if":"else if", num1,op,num2,num1,op,num2,result);
+    ptrdiff_t result = op=='+'?num1+num2:op=='-'?num1-num2:op=='*'?num1*num2:-1;
+    sprintf(resulting_string,"%s(num1 == %td && sign == '%c' && num2 == %td) \n"
+                       "    std::cout << %td << '%c' << %td << \" = \" << %td << std::endl;\n", first?"if":"else if", num1,op,num2,num1,op,num2,result);
     }
     else {
-    double result = op=='+'?num1+num2:op=='-'?num1-num2:op=='*'?num1*num2:op=='/'?num1/static_cast<double>(num2):-1;
-    sprintf(resulting_string,"%s(num1 == %zu && sign == '%c' && num2 == %zu) \n"
-                       "    std::cout << %zu << '/' << %zu << \" = \" << %F << std::endl;\n", first?"if":"else if", num1,op,num2,num1,num2,result);
+    //prevent 0/0
+    double result = num1&&num2?num1/static_cast<double>(num2):0;
+    sprintf(resulting_string,"%s(num1 == %td && sign == '%c' && num2 == %td) \n"
+                       "    std::cout << %td << '/' << %td << \" = \" << %F << std::endl;\n", first?"if":"else if", num1,op,num2,num1,num2,result);
     }
 }
 
 int main() {
-
    constexpr size_t min = 0;
    constexpr size_t max = 50;
    const char signs[] = "+-*/";
@@ -37,8 +37,8 @@ int main() {
    "std::cin >> num2;\n",min,max);
    output_file << output_cpp << '\n';
    for(int i = 0; i < 4; ++i) {
-    for(size_t num1 = min; num1 <= max; ++num1){
-        for(size_t num2 = min; num2 <= max; ++num2){
+    for(ptrdiff_t num1 = min; static_cast<size_t>(num1) <= max; ++num1){
+        for(ptrdiff_t num2 = min; static_cast<size_t>(num2) <= max; ++num2){
         get_formated_string(num1,num2,signs[i],if_statment,!i&&!num1&&!num2);
         output_file << if_statment;
        }
